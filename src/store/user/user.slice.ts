f39fe96f67a022a2626c286a1f37039ef5697df6
Daria Lapteva <d.pekhtereva@ndmsystems.com>
2026-03-10 17:08:45 +0300

    fix login issue

--- src/store/user/user.slice.ts
+++ src/store/user/user.slice.ts
@@ -19,7 +19,7 @@ export const userProcess = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
-      .addCase(checkAuthAction.fulfilled, (state) => {
+      .addCase(checkAuthAction.fulfilled, (state, action) => {
         state.authorizationStatus = AuthorizationStatus.Auth;
         state.userData = action.payload;
 
